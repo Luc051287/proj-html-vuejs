c192fd4844f5bd7dcc457338978cf2df28a6491a
Luca Gentili <l.gentili87@icloud.com>
2021-01-12 15:10:27 +0100

    Header Bottom

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,9 @@
 
 let mix = require('laravel-mix');
 mix.sass('src/app.scss', 'css');
+// mix.sass('src/app.scss', 'dist').options({
+//     processCssUrls: false
+// });
+mix.options({
+    processCssUrls: false // questa opzione fa in modo da non modificare i percorsi delle immagini
+});
