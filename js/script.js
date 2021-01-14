ffc3e699a3bade944ba536397ca97b1bda65390e
Luca Gentili <l.gentili87@icloud.com>
2021-01-14 11:11:04 +0100

    Vue Header (1)

--- js/script.js
+++ js/script.js
@@ -2,7 +2,26 @@ var app = new Vue(
   {
     el:"#root",
     data: {
-
+      indexNavMenu: 0,
+      active_li: "active_li",
+      mainLogo: "logo.png",
+      openingTime: ["clock", "Open hours: Mon - Sat - 9:00 - 18:00"],
+      mainInfos: {
+        phoneNumber: ["#", "phone-alt", "+1 (305) 1234-5678"],
+        email: ["#", "envelope", "hello@example.com"]
+      },
+      social: {
+        facebook: ["#", "facebook-f"],
+        twitter: ["#", "twitter"],
+        linkedin: ["#", "linkedin-in"]
+      },
+      mainNavBar: {
+        home: "#",
+        services: "#",
+        about: "#",
+        projects: "#",
+        results: "#"
+      }
     },
     methods: {
 
